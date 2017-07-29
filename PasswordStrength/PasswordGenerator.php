c19869cf829b76ab3a61bca71051a5e2b0503e84
Sergei Lissovski <sergei.lissovski@gmail.com>
2017-07-29 14:44:04 +0300

    [MPFE-1014] PasswordGenerator service is added

--- PasswordStrength/PasswordGenerator.php
+++ PasswordStrength/PasswordGenerator.php
@@ -5,6 +5,8 @@ namespace Modera\SecurityBundle\PasswordStrength;
 use Modera\SecurityBundle\Entity\User;
 
 /**
+ * @since 2.56.0
+ *
  * @author    Sergei Lissovski <sergei.lissovski@modera.org>
  * @copyright 2017 Modera Foundation
  */
@@ -28,6 +30,21 @@ class PasswordGenerator
      */
     public function generatePassword(User $user = null)
     {
+        while (true) {
+            $plainPassword = '';
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            for ($i = 0; $i < $this->passwordConfig->getMinLength(); ++$i) {
+                $plainPassword .= $characters[rand(0, strlen($characters) - 1)];
+            }
 
+            if ($this->passwordConfig->isNumberRequired() && !preg_match('/[0-9]/', $plainPassword)) {
+                continue;
+            }
+            if ($this->passwordConfig->isCapitalLetterRequired() && !preg_match('/[A-Z]/', $plainPassword)) {
+                continue;
+            }
+
+            return $plainPassword;
+        }
     }
 }
