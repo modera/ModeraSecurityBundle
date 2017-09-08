6696959cd76f0a282dc93bd7b89366e3047245ba
Sergei Vizel <sergei.vizel@modera.net>
2017-09-08 15:44:45 +0300

    [MPFE-1014] Password strength polishing

--- Tests/Unit/DependencyInjection/ConfigurationTest.php
+++ Tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -2,8 +2,9 @@
 
 namespace Modera\SecurityBundle\Tests\Unit\DependencyInjection;
 
-use Modera\SecurityBundle\DependencyInjection\Configuration;
 use Symfony\Component\Config\Definition\Processor;
+use Modera\SecurityBundle\DependencyInjection\Configuration;
+use Modera\SecurityBundle\PasswordStrength\PasswordConfigInterface;
 
 /**
  * @author    Sergei Lissovski <sergei.lissovski@modera.org>
@@ -33,5 +34,55 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
             'rotation_period' => 90,
         );
         $this->assertSame($expectedConfig, $config);
+    }
+
+    public function testPasswordStrengthLetterRequired()
+    {
+        $values = array_merge(
+            array(true, false),
+            PasswordConfigInterface::LETTER_REQUIRED_TYPES,
+            array('on')
+        );
+
+        foreach ($values as $value) {
+            $this->assertPasswordStrengthLetterRequired($value);
+        }
+    }
+
+    private function assertPasswordStrengthLetterRequired($value)
+    {
+        $configuration = new Configuration();
+
+        $processor = new Processor();
+        $config = $processor->processConfiguration($configuration, array(
+            'modera_security' => array(
+                'password_strength' => array(
+                    'letter_required' => $value,
+                ),
+            ),
+        ));
+
+        $expected = false;
+        if (is_bool($value) && $value) {
+            $expected = PasswordConfigInterface::LETTER_REQUIRED_TYPE_CAPITAL_OR_NON_CAPITAL;
+        } else if (is_string($value)) {
+            $expected = $value;
+            if (!in_array($value, PasswordConfigInterface::LETTER_REQUIRED_TYPES)) {
+                $expected = PasswordConfigInterface::LETTER_REQUIRED_TYPE_CAPITAL_OR_NON_CAPITAL;
+            }
+        }
+
+        $expectedConfig = array(
+            'mail' => array(
+                'service' => 'modera_security.password_strength.mail.default_mail_service',
+                'sender' => 'no-reply@no-reply',
+            ),
+            'enabled' => false,
+            'min_length' => 6,
+            'number_required' => false,
+            'letter_required' => $expected,
+            'rotation_period' => 90,
+        );
+        $this->assertEquals($expectedConfig, $config['password_strength']);
     }
 }
