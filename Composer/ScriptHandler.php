325d5da72a243be729f6115a7f51c39264e0bdba
Sergei Lissovski <sergei.lissovski@gmail.com>
2017-11-26 16:16:46 +0200

    [MPFE-1051] all custom composer ScriptHandlers updated to use 'symfony-bin-dir' instead of old SF2.x 'symfony-app-dir'

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -17,18 +17,18 @@ class ScriptHandler extends AbstractScriptHandler
     public static function installPermissions(Event $event)
     {
         $options = static::getOptions($event);
-        $appDir = $options['symfony-app-dir'];
+        $binDir = $options['symfony-bin-dir'];
 
         echo '>>> ModeraSecurityBundle: Install permissions'.PHP_EOL;
 
-        if (!is_dir($appDir)) {
-            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not install permissions.'.PHP_EOL;
+        if (!is_dir($binDir)) {
+            echo 'The symfony-app-dir ('.$binDir.') specified in composer.json was not found in '.getcwd().', can not install permissions.'.PHP_EOL;
 
             return;
         }
 
-        static::executeCommand($event, $appDir, 'modera:security:install-permission-categories', $options['process-timeout']);
-        static::executeCommand($event, $appDir, 'modera:security:install-permissions', $options['process-timeout']);
+        static::executeCommand($event, $binDir, 'modera:security:install-permission-categories', $options['process-timeout']);
+        static::executeCommand($event, $binDir, 'modera:security:install-permissions', $options['process-timeout']);
 
         echo '>>> ModeraSecurityBundle: done'.PHP_EOL;
     }
