7e1f18c6659ea4b86e860a10ec06b8b943762316
Sergei Vizel <sergei.vizel@modera.net>
2018-11-08 15:38:56 +0200

    Routing improvements

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace Modera\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
